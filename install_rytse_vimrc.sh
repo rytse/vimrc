4f3f114686770729c54e0ccbf87f3cde15677b5f
Ryan Tse <ryantse100@gmail.com>
2020-09-08 12:01:31 -0400

    Use https git targets for portability

--- install_rytse_vimrc.sh
+++ install_rytse_vimrc.sh
@@ -18,13 +18,13 @@
  echo "Finished installing amix's Awesome Ultimate Vim configuration! Moving on to rytse's custom stuff. Enjoy :-)"
 
  # vim tmux seamless navigation
- git clone git@github.com:christoomey/vim-tmux-navigator.git my_plugins/vim-tmux-navigator
+ git clone https://github.com/christoomey/vim-tmux-navigator.git my_plugins/vim-tmux-navigator
 
  # YCM code completion for Python, C-like, and Rust
  sudo apt install libclang-8-dev
  git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
  git clone https://github.com/nvie/vim-flake8.git
- git clone git@github.com:ycm-core/YouCompleteMe.git my_plugins/YouCompleteMe
+ git clone https://github.com:ycm-core/YouCompleteMe.git my_plugins/YouCompleteMe
  cd my_plugins/YouCompleteMe
  git submodule update --init --recursive
  python3 install.py --clang-completer --rust-completer
